7848d4fa6d05f654400cafc403e9174b466cf551
Justinjp126 <39039227+Justinjp126@users.noreply.github.com>
2021-01-17 12:16:36 -0500

    Finish home page - add form validation, fix quotes, and add footer + twitter/insta

--- js/index.js
+++ js/index.js
@@ -1,9 +1,38 @@
-document.addEventListener(
-    'DOMContentLoaded',
-    () => {
-      const scroller = new SweetScroll({
-        /* some options */
+function checkValid() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+
+  if (name !== "" && validateEmail(email)) {
+    Swal.fire({
+      title: "Success!",
+      text: "Your daily briefing will be in you inbox every day",
+      icon: "success",
+      confirmButtonText: "Thank You",
+    }).then((result) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      location.reload();
+    });
+  } else {
+    if (name == "") {
+      Swal.fire({
+        title: "Error!",
+        text: "Please fill out your name",
+        icon: "error",
+        confirmButtonText: "Continue",
       });
-    },
-    false,
-  );
+    } else if (!validateEmail(email)) {
+      Swal.fire({
+        title: "Error!",
+        text: "Please enter valid email address",
+        icon: "error",
+        confirmButtonText: "Continue",
+      });
+    }
+  }
+}
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
