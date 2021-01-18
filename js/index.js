b8fa3248ff42d50d853f4a1f902fb66eae7717fc
Justinjp126 <39039227+Justinjp126@users.noreply.github.com>
2021-01-18 08:55:44 -0500

    add html for jack

--- js/index.js
+++ js/index.js
@@ -36,3 +36,11 @@ function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
+
+function findSymptoms() {
+  document.querySelectorAll("input").forEach(function (element) {
+    if (element.checked) {
+      console.log("Symptoms " + element.id);
+    }
+  });
+}
