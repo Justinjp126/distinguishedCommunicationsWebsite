aa2252ecab104b8e37eeae316f7be812c524d6a2
Justinjp126 <39039227+Justinjp126@users.noreply.github.com>
2021-03-21 19:59:18 -0400

    Add button click effect for cart

--- js/index.js
+++ js/index.js
@@ -4,6 +4,29 @@
 var checkedList = [];
 
 /**
+ * Ripple effect function
+ */
+function createRipple(event) {
+  const button = event;
+  const circle = document.createElement("span");
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+  circle.classList.add("ripple");
+
+  const ripple = button.getElementsByClassName("ripple")[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+}
+
+/**
  * Checks if contact form info is valid
  */
 function checkValid() {
@@ -1056,8 +1079,6 @@ function addToCart() {
   var cartButtons = document.querySelectorAll(".cart-check-container");
 
   cartButtons.forEach((service) => {
-    console.log(service);
-
     service.addEventListener("click", (event) => {
       if (localStorage.getItem("cart") == null) {
         localStorage.setItem("cart", []);
