47f9f7cacabe3c4af0ce6e3b54b3796f3d390feb
Justinjp126 <39039227+Justinjp126@users.noreply.github.com>
2021-04-26 23:43:30 -0400

    Start adding subtle animations

--- js/gsap.js
+++ js/gsap.js
@@ -3,11 +3,59 @@ document.addEventListener("DOMContentLoaded", function (event) {
   window.onload = function () {
     var timeline = gsap.timeline({ defaults: { duration: 1 } });
 
+    gsap.registerPlugin(ScrollTrigger);
+
     timeline.from(".homeImg__title_items", {
       opacity: 0,
       x: -50,
       duration: 0.9,
       stagger: 0.1,
+    });
+
+    gsap.from(".quote", {
+      opacity: 0.1,
+      x: -25,
+      scrollTrigger: {
+        trigger: ".homeImg__button",
+        start: "bottom bottom-=100",
+        markers: true,
+        scrub: true,
+      },
+    });
+
+    gsap.from(".featured__title", {
+      opacity: 0.7,
+      scrollTrigger: {
+        trigger: ".featured__title",
+        start: "bottom bottom",
+        end: "+=100px",
+        markers: true,
+        scrub: true,
+      },
+    });
+
+    gsap.from(".card-title", {
+      opacity: 0.1,
+      y: -10,
+      scrollTrigger: {
+        trigger: ".card-title",
+        start: "bottom bottom",
+        end: "+=100px",
+        markers: true,
+        scrub: true,
+      },
+    });
+
+    gsap.from(".card-text", {
+      opacity: 0.1,
+      y: -10,
+      scrollTrigger: {
+        trigger: ".card-title",
+        start: "bottom bottom",
+        end: "+=100px",
+        markers: true,
+        scrub: true,
+      },
     });
   };
 });
